c40eb68ced55fe2cfcda79d37a267263ce02685b
KirillRV <kirillreges639@gmail.com>
2025-04-03 17:27:32 +0300

    HW 43-45

--- TMS_HW/src/main/java/com/tms/Main.java
+++ TMS_HW/src/main/java/com/tms/Main.java
@@ -1,8 +1,27 @@
 package com.tms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+        title = "Spring Boot REST Market",
+        version = "1.0",
+        description = "This is a simple market application on Spring Boot with REST API.  This API allows users to...",
+        termsOfService = "https://reges.com/terms",
+        contact = @Contact(
+                name = "Kirill Reges",
+                url = "https://www.reges.com/contact",
+                email = "kirillreges639@gmail.com"
+        ),
+        license = @License(
+                name = "Apache 2.0",
+                url = "https://www.apache.org/licenses/LICENSE-2.0"
+        )
+))
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
